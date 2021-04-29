b3d4a362c4475b7d78934cb84417ec6142a8912b
Mohd Faiz Ansari <mohdfaiz.afk@gmail.com>
2021-04-29 11:43:30 +0530

    Implemented Aggregate Pipelines method of MongoDB

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const { Tour } = require('../models/tourModel');
-const APIFeatures = require('../utils/apifeatures')
+const APIFeatures = require('../utils/apifeatures');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -98,3 +98,85 @@ exports.deleteTour = async (req, res) => {
     res.send(err);
   }
 };
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      { 
+        $match: { ratingsAverage: { $gte: 4.5 } } // we can match multiple data
+      },
+      { 
+        $group: { 
+        _id: {$toUpper:'$difficulty'},
+          numTours: {$sum: 1},
+          numRatings: {$sum : '$ratingsQuantity'},
+          avgRating:{$avg:'$ratingsAverage'},
+          avgPrice:{$avg : '$price' },
+          minPrice:{$min : '$price' },
+          maxPrice:{$max : '$price' }
+        } 
+      },{
+        $sort:{avgPrice: 1} // 1 for ascending order
+      }
+    ]);
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        tour: stats
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'Fail',
+      message: err.message,
+    });
+  }
+};
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-01`)
+          }
+        }
+      },
+      {
+        $group: {
+          _id:{ $month: '$startDates'},
+          numTourStarts: { $sum: 1} ,// 1 for each documents
+          tours: { $push: '$name'}
+        }
+      },
+      {
+        $addFields: { month: '$_id'}
+      },
+      {
+        $project:{_id:0}// 0 to not show the id field
+      },
+      {
+        $sort: { numTourStarts: -1} // 1 sort in ascending order and -1 for descending order
+      },
+      {
+        $limit: 12  // limit the documents to be displayed
+      }
+    ]);
+    res.status(200).json({
+      status: 'Success',
+      data: {
+         plan
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'Fail',
+      message: err.message,
+    });
+  }
+}
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,7 +3,9 @@ const tourController = require('../controllers/tourController')
 
 const router = express.Router();
 
-router.route('/top-5-cheap').get(tourController.aliasTopTours ,tourController.getAllTours)
+router.route('/top-5-cheap').get(tourController.aliasTopTours ,tourController.getAllTours);
+router.route('/tour-stats').get(tourController.getTourStats)
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan)
 
 router
 .route('/')
